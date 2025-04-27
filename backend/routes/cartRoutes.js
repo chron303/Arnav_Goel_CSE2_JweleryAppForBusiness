fc7b40388dec67ff7f233a7444b8a1513ca4a395
chron303 <goelarnav30@gmail.com>
2025-04-27 18:32:09 +0530

    Initial project upload with LFS tracking

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const { addToCart } = require("../controllers/cartController");
+const router = express.Router();
+router.post("/add", addToCart);
+module.exports = router;
